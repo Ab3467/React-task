77cbc66360d86d10230343409f04ec5e09356b14
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-07-12 11:15:32 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className=""></div>
+      <div className="flex flex-row items-center justify-center min-h-screen">
+        <div className="container mx-auto px-4 py-8">
+          <h1 className="text-3xl font-bold text-blue-600 mb-4 font-serif"></h1>
+        </div>
+      </div>
     </>
   );
 }
